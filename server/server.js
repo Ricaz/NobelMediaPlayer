b691dc8cb9be61a8afd0288fb648ee7778fb86ee
Martin Alstrup <martin@rcz.dk>
2014-06-23 14:08:48 +0200

    prettified output

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var util =      require('util');
 var exec =      require('child_process').exec;
 var srv =       require('socket.io').listen(8080);
 var Mopidy =    require('mopidy');
+var colors =    require('colors');
 
 var password = 1337;
 var revision;
@@ -13,12 +14,10 @@ var mopidy = new Mopidy({
 	autoConnect: true
 });
 
-srv.set('log level', 0);
 var consoleError = console.error.bind(console);
 
 exec("svn info " + __dirname +  " | grep 'Revision: ' | cut -d' ' -f2", function(err, stdout, stderr) {
     revision = stdout.replace(/[^0-9]/g, "");
-    console.log('Revision: ', revision);
 });
 
 srv.sockets.on('connection', function (socket) {
@@ -26,30 +25,34 @@ srv.sockets.on('connection', function (socket) {
     socket.adminmode = false;
     socket.ip = socket.handshake.address.address + ":" + socket.handshake.address.port;
 
-    console.log('Client connected from:', socket.ip, ". Assigning ID:", socket.id);
+    console.log('['.yellow, socket.id.white, ']'.yellow, 'Client connected from:'.green, socket.ip);
 
-    console.log('Sending revision: ', revision);
+    console.log('['.yellow, socket.id.white, ']'.yellow, 'Sending revision.');
     socket.emit('revision', revision);
 
     socket.on('request-revision', function() {
         socket.emit('revision', revision);
     });
 
+    socket.on('disconnect', function() {
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'Disconnected.'.red);
+    });
+
     socket.on('request-admin', function(pass) {
-        console.log(socket.id + ' sent pass: ', pass);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'sent pass: ', pass);
         if (pass == password) socket.adminmode = true;
         else socket.adminmode = false;
-        console.log('Sending adminmode ', socket.adminmode, ' to client ', socket.id);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'Sending adminmode ', socket.adminmode, 'to client.');
         socket.emit('adminmode', socket.adminmode);
     });
 
     socket.on('request-shuffle',function() {
-        console.log(socket.id + ' wants to shuffle.');
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to shuffle.');
         mopidy.tracklist.shuffle(1);
     });
 
     socket.on('request-current', function(e, data) {
-        console.log(socket.id + ' asked for current song.');
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'asked for current song.');
         // Because this is done asynchronously, we 'bind' a function to be called when we get a response from Mopidy.
         mopidy.playback.getCurrentTlTrack().then(
             function (track) {
@@ -59,7 +62,7 @@ srv.sockets.on('connection', function (socket) {
     });
 
 	socket.on('request-tracklist', function (e, data) {
-		console.log(socket.id + ' asked for current tracklist.');
+		console.log('['.yellow, socket.id.white, ']'.yellow, 'asked for current tracklist.');
         mopidy.tracklist.getTlTracks().then(
             function(tracks) {
                 socket.emit('tracklist', tracks);
@@ -68,7 +71,7 @@ srv.sockets.on('connection', function (socket) {
 	});
 
     socket.on('request-clear', function (data) {
-        console.log(socket.id + ' wants to clear tracklist.');
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to clear tracklist.');
         mopidy.tracklist.clear().then(
             mopidy.tracklist.getTlTracks().then(
                 function(tracks) {
@@ -84,7 +87,7 @@ srv.sockets.on('connection', function (socket) {
     });
 
     socket.on('request-playlists', function (data) {
-        console.log(socket.id + ' requested playlists.');
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'requested playlists.');
         mopidy.playlists.getPlaylists().then(
             function(playlists) {
                 socket.emit('playlists', playlists);
@@ -93,7 +96,7 @@ srv.sockets.on('connection', function (socket) {
     });
 
     socket.on('request-add-playlist', function(data) {
-        console.log(socket.id + ' requested to load playlist: ' + data.uri);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'requested to load playlist: ' + data.uri);
         mopidy.tracklist.add(null, null, data).then(
             mopidy.tracklist.getTlTracks().then(
                 function(tracks) {
@@ -103,17 +106,18 @@ srv.sockets.on('connection', function (socket) {
         )
     });
 
+    // currently not in use, no support from Mopidy
     socket.on('request-add-playlist-to-account', function(uri) {
-        console.log(socket.id + ' requested to add playlist:', uri);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'requested to add playlist:', uri);
         mopidy.playlists.lookup(uri).then(
             function (playlist) {
-                console.log(socket.id + ' wants to add:', playlist);
+                console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to add: ' + playlist);
             }
         );
     });
 
     socket.on('request-playlist', function (data) {
-        console.log(socket.id + ' requested playlist: ' + data.uri);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'requested playlist: ' + data.uri);
         mopidy.playlists.lookup(data).then(
             function (playlist) {
                 socket.emit('playlist', playlist);
@@ -124,7 +128,7 @@ srv.sockets.on('connection', function (socket) {
     // very messy :(
     // Basically: if song is added and nothing is currently playing, play first song in tracklist.
     socket.on('request-add-song', function(uri) {
-        console.log(socket.id + ' requested to add: ' + uri);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'requested to add: ' + uri);
         var exists = false;
         mopidy.tracklist.getTracks().then(
             function(tracks) {
@@ -135,7 +139,7 @@ srv.sockets.on('connection', function (socket) {
                     }
                 }
                 if (!exists) {
-                    console.log('Track is not in tracklist, adding...');
+                    console.log('['.yellow, '        SERVER      '.white, ']'.yellow, 'Track not in tracklist, adding...');
                     mopidy.tracklist.add(null, null, uri).then(
                         function() {
                             mopidy.tracklist.getTlTracks().then(
@@ -164,10 +168,10 @@ srv.sockets.on('connection', function (socket) {
     });
 
 	socket.on('request-song-change', function (data) {
-		console.log(socket.id + ' wants to change song to: ', data);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to change song to:', data);
 
         mopidy.tracklist.index(data).then(function(i) {
-            console.log('index:', i);
+            console.log('['.yellow, '        SERVER      '.white, ']'.yellow, 'Index:', i);
             mopidy.tracklist.move(i,i,1).then(function() {
                 mopidy.tracklist.slice(0, 1).then(
                     function (slice) {
@@ -175,7 +179,7 @@ srv.sockets.on('connection', function (socket) {
                         for (var i = 0; i < slice.length; i++) {
                             slicetlid.push(slice[i].tlid);
                         }
-                        console.log('Removing:', slicetlid);
+                        console.log('['.yellow, '        SERVER      '.white, ']'.yellow, 'Removing:', slicetlid);
                         mopidy.tracklist.remove({tlid: slicetlid});
                         mopidy.playback.play(data);
                     }, consoleError
@@ -186,7 +190,7 @@ srv.sockets.on('connection', function (socket) {
 	});
 
     socket.on('request-remove', function (track) {
-        console.log(socket.id + ' wants to remove:', track.tlid);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to remove:', track.tlid);
         var arr = [];
         arr.push(track.tlid);
         mopidy.tracklist.remove({tlid: arr});
@@ -205,7 +209,7 @@ srv.sockets.on('connection', function (socket) {
     });
 
     socket.on('request-pause', function () {
-        console.log(socket.id + ' wants to pause.');
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to pause.');
         mopidy.playback.pause().then(function() {
                 socket.emit('song-pause')
             }, consoleError
@@ -213,7 +217,7 @@ srv.sockets.on('connection', function (socket) {
     });
 
 	socket.on('request-next', function (e, data) {
-		console.log(socket.id + ' wants to skip to next song.');
+		console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to skip to next song.');
         // First ask mopidy to change song. On success, send back the new song.
         mopidy.playback.getCurrentTlTrack().then(
             function(prevTrack) {
@@ -230,7 +234,7 @@ srv.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('request-previous', function (e, data) {
-		console.log(socket.id + ' wants to play previous song.');
+		console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to play previous song.');
         mopidy.playback.previous().then(
             mopidy.playback.getCurrentTlTrack().then(
                 function (track) {
@@ -241,6 +245,7 @@ srv.sockets.on('connection', function (socket) {
 	});
 
     socket.on('request-current-time', function () {
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'requested current time.');
         mopidy.playback.getTimePosition().then(
             function (milliseconds) {
                 socket.emit('current-time', milliseconds);
@@ -249,10 +254,10 @@ srv.sockets.on('connection', function (socket) {
     });
 
     socket.on('request-search', function (data) {
-        console.log('Received search string:', data);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'Received search string: ', data);
         mopidy.library.search({any: data}, uris=['spotify:']).then(
             function (result) {
-                console.log('Received search result!');
+                console.log('['.yellow, '        SERVER      '.white, ']'.yellow, 'Received search result.');
                 if (result) {
                     var tracks = result[0].tracks;
                     socket.emit('search-result', tracks);
@@ -264,14 +269,14 @@ srv.sockets.on('connection', function (socket) {
     });
 
     socket.on('request-set-volume', function (volume) {
-        console.log(socket.id + ' wants to set volume:', volume);
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to set volume:', volume);
         mopidy.playback.setVolume(volume);
     });
 
     socket.on('request-volume', function () {
-        console.log(socket.id + ' wants to get volume.');
+        console.log('['.yellow, socket.id.white, ']'.yellow, 'wants to get volume.');
         var vol = mopidy.playback.getVolume().then(function (volume) {
-            console.log('Sending volume ' + volume + ' to client ' + socket.id);
+            console.log('['.yellow, socket.id.white, ']'.yellow, 'Sending volume ' + volume + ' to client.');
             socket.emit('volume', volume);
         });
     });
@@ -279,85 +284,52 @@ srv.sockets.on('connection', function (socket) {
 });
 
 mopidy.on('state:online', function() {
-	console.log('Connected to Mopidy.');
+	console.log('Connected to Mopidy.'.green);
     mopidy.tracklist.setConsume(true);
 
     mopidy.on('event:trackPlaybackStarted', function (track, time) {
-        console.log('Mopidy: Playback started.');
+        console.log('['.yellow, '        Mopidy      '.white, ']'.yellow, 'Playback started.');
+        console.log('['.yellow, '     All clients    '.white, ']'.yellow, 'Sending playback started.');
         track = track.tl_track;
-        srv.sockets.clients().forEach(function (socket) {
-            console.log('Sending current and resume to client ' + socket.id);
-            socket.emit('current', track);
-            socket.emit('song-resume', time);
-        });
-
-        /*mopidy.tracklist.index(track).then(
-            function (index) {
-                console.log('Attempting to slice from 0 to', index);
-                mopidy.tracklist.slice(0, index).then(
-                    function (slice) {
-                        var slicetlid = [];
-                        for (var i = 0; i < slice.length; i++) {
-                            slicetlid.push(slice[i].tlid);
-                        }
-                        console.log('Removing:', slicetlid);
-                        mopidy.tracklist.remove({tlid: slicetlid});
-                        *//**//*console.log('Songs to remove:', slice.length);
-                        for (var i = 0; i < slice.length; i++) {
-                            console.log('Removing', slice[i].tlid);
-                            mopidy.tracklist.remove({tlid: slice[i].tlid});
-                        }*//**//*
-                    }, consoleError
-                );
-            }
-        );*/
-
+        srv.sockets.emit('current', track);
+        srv.sockets.emit('song-resume', time);
     });
 
     mopidy.on('event:trackPlaybackPaused', function (track, time) {
-        console.log('Mopidy: Playback paused.');
-        srv.sockets.clients().forEach(function (socket) {
-            console.log('Sending pause to client ' + socket.id);
-            socket.emit('song-pause');
-        });
-
+        console.log('['.yellow, '        Mopidy      '.white, ']'.yellow, 'Playback paused.');
+        srv.sockets.emit('song-pause');
+        console.log('['.yellow, '     All clients    '.white, ']'.yellow, 'Sending pause.');
     });
 
     mopidy.on('event:trackPlaybackResumed', function (track, time) {
-        console.log('Mopidy: Playback resumed.');
-        srv.sockets.clients().forEach(function (socket) {
-            console.log('Sending resumed to client ' + socket.id);
-            socket.emit('song-resume', time);
-        });
+        console.log('['.yellow, '        Mopidy      '.white, ']'.yellow, 'Playback resumed.');
+        console.log('['.yellow, '     All clients    '.white, ']'.yellow, 'Sending resumed.');
+        srv.sockets.emit('song-resume', time);
     });
 
     mopidy.on('event:seeked', function (time) {
-        console.log('Mopidy: Playback seeked.');
-        srv.sockets.clients().forEach(function (socket) {
-            console.log('Sending seeked to client ' + socket.id);
-            socket.emit('current-time', time.time_position);
-        });
+        console.log('['.yellow, '        Mopidy      '.white, ']'.yellow, 'Playback seeked.');
+        console.log('['.yellow, '     All clients    '.white, ']'.yellow, 'Sending seek.');
+        srv.sockets.emit('current-time', time.time_position);
     });
 
     mopidy.on('event:tracklistChanged', function () {
+        console.log('['.yellow, '        Mopidy      '.white, ']'.yellow, 'Tracklist changed.');
         mopidy.tracklist.getTlTracks().then(
             function (tracks) {
-                srv.sockets.clients().forEach(function (socket) {
-                    console.log('Sending tracklist to client ' + socket.id);
-                    socket.emit('tracklist', tracks);
+                console.log('['.yellow, '     All clients    '.white, ']'.yellow, 'Sending tracklist.');
+                    srv.sockets.emit('tracklist', tracks);
                 });
             }, consoleError
-        );
     });
 
     mopidy.on('event:volumeChanged', function(volume) {
-        srv.sockets.clients().forEach(function (socket) {
-            console.log('Sending volume to client ' + socket.id);
-            socket.emit('volume', volume);
-        });
+        console.log('['.yellow, '        Mopidy      '.white, ']'.yellow, 'Volume changed.');
+        console.log('['.yellow, '     All clients    '.white, ']'.yellow, 'Sending volume.');
+        srv.sockets.emit('volume', volume);
     })
 });
 
 mopidy.on('state:offline', function() {
-	console.log('Disconnected from Mopidy.');
+	console.log('Disconnected from Mopidy.'.red);
 });
